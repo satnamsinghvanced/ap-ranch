e9631430e2488e42b6f4f19a6779a20fc36a4479
satnamsinghvancedsolutions <SanjayKumar@vancedsolutions.com>
2024-09-03 14:40:06 +0530

    ui chnages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,54 +15,51 @@ const Home = () => {
       <Header />
 
       <section className="apr-banner section-padding">
-        <div className="d-flex justify-content-center w-100 align-items-center h-100 animate__animated animate__fadeInDown">
+        <div className="d-flex justify-content-center w-100 align-items-center h-100 ">
           <img src={aprSxLogo} alt="" className="img-fluid" />
         </div>
       </section>
 
       <section className=" about-apr section-pending">
-        <div className="max-width-1000 mx-auto text-center animate__backInUp animate__animated">
+        <div className="max-width-1000 mx-auto text-center ">
           <p>
             Our mission at AP Ranch is to create a transformative sports
             facility and community dedicated to cultivating and empowering
             student-athletes of all backgrounds and abilities. We strive to
             provide a nurturing environment where INDIVIDUALS can explore their
-            passions, develop skills, and unlock their true potential.
-            <h1 data-aos="zoom-y-out" data-aos-delay="50">
-              Sample heading
-            </h1>
+            passions, develop skills, and unlock their true potential. 
           </p>
         </div>
       </section>
       <section className="container-fluid">
         <div className="row">
-          <div className="col-md-4 p-0 ">
-            <div className="foodball-img sport-img-box animate__animated animate__fadeInLeft">
+          <div className="col-lg-4 col-md-6 p-0 ">
+            <div className="foodball-img sport-img-box ">
               <div className="d-flex align-items-center justify-content-center flex-column h-100">
                 <h3 className="sport-name-heading">FOOTBALL</h3>
                 <button className="explore-btn  mt-4">EXPLORE</button>
               </div>
             </div>
           </div>
-          <div className="col-md-4 p-0">
-            <div className="basketball-img sport-img-box animate__backInDown animate__animated ">
+          <div className="col-lg-4 col-md-6 p-0">
+            <div className="basketball-img sport-img-box  ">
               <div className="d-flex align-items-center justify-content-center flex-column h-100 ">
                 <h3 className="sport-name-heading">BASKETBALL</h3>
                 <button className="explore-btn  mt-4">EXPLORE</button>
               </div>
             </div>
           </div>
-          <div className="col-md-4 p-0 ">
-            <div className="track-img sport-img-box animate__animated animate__fadeInRight">
+          <div className="col-lg-4 col-md-6 p-0 ">
+            <div className="track-img sport-img-box ">
               <div className="d-flex align-items-center justify-content-center flex-column h-100">
                 <h3 className="sport-name-heading">TRACK</h3>
                 <button className="explore-btn  mt-4">EXPLORE</button>
               </div>
             </div>
           </div>
-          <div className="col-md-12 p-0">
-            <div className="the-facility-img-box">
-              <div className="d-flex align-items-center justify-content-center flex-column h-100">
+          <div className="col-lg-12  col-md-6 p-0">
+            <div className="the-facility-img-box ">
+              <div className="d-flex align-items-center justify-content-center flex-column h-100 text-center-md">
                 <h3 className="sport-name-heading font-50">the facility</h3>
                 <button className="explore-btn  mt-4">EXPLORE</button>
               </div>
@@ -74,21 +71,21 @@ const Home = () => {
         <h3 className="incredible-partners-heading text-center">
           our incredible partners
         </h3>
-        <h1 data-aos="zoom-y-out">Sample heading</h1>
+
         <div className="incredible-partners-logos">
-          <div>
+          <div className="text-center" >
             <img src={nikeLogo} alt="" />
           </div>
-          <div>
+          <div className="text-center" >
             <img src={rekortan} alt="" />
           </div>
-          <div>
+          <div className="text-center">
             <img src={USATrackLogo} alt="" />
           </div>
-          <div>
+          <div className="text-center">
             <img src={sorinexLogo} alt="" />
           </div>
-          <div>
+          <div className="text-center">
             <img src={artboardLogo} alt="" />
           </div>
         </div>
@@ -96,19 +93,19 @@ const Home = () => {
 
       <section className="click-to-donate  container-fluid">
         <div className="row">
-          <div className="col-md-6">
-            <div className="d-inline-flex w-auto justify-content-center h-100 flex-column click-to-donate-content  ps-5 animate__backOutLeft animate__animated">
-              <h1 className="click-to-donate-heading ms-5">
+          <div className="col-lg-6 pb-5">
+            <div className="d-inline-flex w-auto justify-content-center h-100 flex-column click-to-donate-content  ">
+              <h1 className="click-to-donate-heading">
                 CLICK to
                 <br /> donate
               </h1>
-              <button className="donate-white-btn border-0 mt-3  ms-5">
+              <button className="donate-white-btn border-0 mt-3  ">
                 DONATE
               </button>
             </div>
           </div>
-          <div className="col-md-6 p-0">
-            <div className="click-to-donate-box animate__backOutRight animate__animated"></div>
+          <div className="col-lg-6 p-0 d-done-sm">
+            <div className="click-to-donate-box"></div>
           </div>
         </div>
       </section>
