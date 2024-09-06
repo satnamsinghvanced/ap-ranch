f8738c37432009ed116f6c1a87c63d8a904286c3
satnamsinghvancedsolutions <SanjayKumar@vancedsolutions.com>
2024-09-06 17:00:55 +0530

    Ui changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -38,7 +38,7 @@ const About = () => {
             <div className="container-fluid">
               <div className="row">
                 <div className="col-ms-12 p-0">
-                <div className="about-football-banner">
+                  <div className="about-football-banner">
                     <div className="container h-100">
                        <div className="d-flex align-items-center justify-content-center h-100 flex-wrap">
                         <h1 className="about-page-accordian">FOOTBALL</h1>
@@ -58,8 +58,49 @@ const About = () => {
                         </div>
                        </div>
                     </div>
-                </div>
+                  </div>
 
+                  <div className="about-football-banner-open">
+                    <div className="container pt-3 px-4">
+                      <div className="row">
+                          <div className="col-md-12 pt-5">
+                          <div className="d-flex align-items-center flex-wrap">
+                            <h1 className="about-page-accordian">FOOTBALL</h1>
+                            <div className="ms-auto">
+                             <svg
+                               width="38"
+                               height="38"
+                               viewBox="0 0 38 38"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                             >
+                               <path
+                                 d="M19 0C8.51637 0 0 8.51637 0 19C0 29.4836 8.51637 38 19 38C29.4836 38 38 29.4836 38 19C38 8.51637 29.4836 0 19 0ZM19 1.65217C28.5907 1.65217 36.3478 9.40928 36.3478 19C36.3478 28.5907 28.5907 36.3478 19 36.3478C9.40928 36.3478 1.65217 28.5907 1.65217 19C1.65217 9.40928 9.40928 1.65217 19 1.65217ZM18.1739 9.08696V18.1739H9.08696V19.8261H18.1739V28.913H19.8261V19.8261H28.913V18.1739H19.8261V9.08696H18.1739Z"
+                                 fill="#F2F2F2"
+                               />
+                              </svg>
+                              </div>
+                           </div>
+                          </div>
+                          <div className="col-lg-6 mt-5">
+                              <p className="about-accoridan-text">
+                                <b> At AP Ranch,</b> we believe that football is more than just a game; it’s an opportunity to develop important life skills like teamwork, leadership, and perseverance. By providing a supportive and inclusive environment, we aim to help our student athletes grow into confident and well-rounded individuals who are prepared to succeed in all aspects of life.
+                              </p>
+                          </div>
+                          <div className="col-lg-5 mt-5">
+                            <div className="about-accoridan-services">
+                                <h3 className="about-accoridan-services-heading">Services</h3>
+                                <p className="about-accoridan-services-para">Private One-On-One Training<br></br>
+                                    Small Group Training<br></br>Offensive Mentorship/Consulting<br></br>
+                                    Film Breakdown/ Game Analysis
+                                      
+                                </p>
+                                <button className="explore-btn-transparent">explore</button>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>  
                 </div>
 
                 <div className="col-ms-12 p-0">
