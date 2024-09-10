4897ec9c1763cbb325a1298f87fd7287da27ce3c
satnamsinghvancedsolutions <SanjayKumar@vancedsolutions.com>
2024-09-10 14:57:01 +0530

    Ui changes

--- src/components/about/code-of-conduct.tsx
+++ src/components/about/code-of-conduct.tsx
@@ -1,11 +1,105 @@
 import React from "react";
 import Header from "../layout/header";
+import Footer from "../../pages/Footer";
 
 const CodeOfConductForm = () => {
   return (
     <div>
       <Header />
-      CodeOfConductForm
+        <section className="code-of-conduct">
+        <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2 className="indemnity-heading">Indemnity Agreement
+                </h2>
+                
+                <form action="" className="pt-5 row">
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Printed Name of Student/Participant</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Course/Activity</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Course Number (if applicable)</label>
+                      <input type="date" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Instructor/Sponsor</label>
+                      <input type="dob" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Destination (if travel required) (required)</label>
+                      <input type="dob" className="contact-from-input" />
+                    </div>
+                  </div>
+                  
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Semester/Dates of Participation</label>
+                      <input type="dob" className="contact-from-input" />
+                    </div>
+                  </div>
+                 
+                  <div className="col-md-12">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Email (required)</label>
+                      <input type="dob" className="contact-from-input" />
+                    </div>
+                  </div>
+                </form>
+                <p className="about-indemnity mb-3">For and in consideration of being permitted to participate in the Course/Activity above (including related travel, if any), I, THE UNDERSIGNED, HEREBY FULLY RELEASE AND FOREVER DISCHARGE AP Ranch and all instructors, sponsors, agents, employees, officers, trustees and affiliates of AP Ranch (collectively "AP RANCH"), of and from any and all liability to me, my personal representatives, assigns, heirs and next of kin, for any damage to or loss of my property, any injury to my person or my death or any one or more of the foregoing, arising directly or indirectly out of my participation for any purpose in the Course/Activity, INCLUDING ANY SUCH DAMAGE, LOSS OR INJURY THAT IS CAUSED BY ANY ACT OR OMISSION ON THE PART OF AP RANCH, INCLUDING ANY NEGLIGENT CONDUCT OF AP RANCH but excluding any gross negligence or willful misconduct of AP RANCH. IT IS MY EXPRESS INTENT THAT THE ABOVE RELEASE INCLUDES THE RELEASE BY ME OF AP RANCH FROM THE CONSEQUENCES OF AP RANCH'S OWN NEGLIGENCE. THE ONLY CIRCUMSTANCE UNDER WHICH MY RELEASE OF AP RANCH DOES NOT APPLY IS WITH RESPECT TO ANY OCCURRENCE RESULTING FROM THE GROSS NEGLIGENCE OR WILLFUL MISCONDUCT OF AP RANCH.</p>
+                <p className="about-indemnity mb-3">I FURTHER AGREE TO INDEMNIFY, DEFEND, AND HOLD HARMLESS AP RANCH for, from and against any and all liabilities, damages, claims, lawsuits, costs (including court costs, attorneys fees and costs of investigation), and actions of any kind or description for any damage to or loss of my property or the property of another, any injury to me or my death, or the injury to or death of any other person or any one or more of the foregoing, arising out of my participation for any purpose in the Course/Activity, INCLUDING ANY DAMAGE, LOSS OR INJURY CAUSED BY ANY ACT OR OMISSION ON THE PART OF AP RANCH, INCLUDING ANY NEGLIGENT CONDUCT OF AP RANCH but excluding any gross negligence or willful misconduct of AP RANCH. IT IS MY EXPRESS INTENT THAT THE ABOVE INDEMNITY INCLUDES INDEMNIFICATION BY ME OF AP RANCH FROM THE CONSEQUENCES OF AP RANCH'S OWN NEGLIGENCE. THE ONLY CIRCUMSTANCE UNDER WHICH MY OBLIGATION TO INDEMNIFY AP RANCH DOES NOT APPLY IS WITH RESPECT TO AN OCCURRENCE RESULTING FROM THE GROSS NEGLIGENCE OR WILLFUL MISCONDUCT OF AP RANCH.</p>
+                <p className="about-indemnity mb-3">By execution below I hereby acknowledge that I recognize and assume all of the risks associated with the Course/Activity. I ACKNOWLEDGE THAT IT IS IMPORTANT THAT I VERIFY THAT I HAVE INSURANCE COVERAGE WHICH EXTENDS TO ME WHILE PARTICIPATING IN THE COURSE/ACTIVITY, AND THAT I SECURE SUCH COVERAGE IF I DO NOT ALREADY HAVE IT. I understand that AP RANCH does not provide such coverage, and that no insurance coverage may exist through AP RANCH to cover any injuries or damages which I may sustain or claims which may arise as a result of my participation in the Course/Activity.</p>
+                <p className="about-indemnity mb-3">The terms of this Release and Indemnity Agreement are to be governed by and construed under the laws of the State of Texas, and venue with respect to any dispute arising between AP RANCH and any other party that involves this Release and Indemnity Agreement or my participation in the Course/Activity shall be exclusively in Tarrant County, Texas.</p>
+                <p className="about-indemnity mb-3">Each provision of this Release and Indemnity Agreement is severable and if one portion is invalid or illegal, such invalid or illegal portion shall not apply, but the remaining portions shall nevertheless remain in full force and effect. I understand that the terms of the Release and Indemnity Agreement are contractual and not mere recitals, and that such terms are binding upon me, my heirs, personal representatives and assigns.</p>
+                <p className="about-indemnity mb-3">In making this Release and Indemnity Agreement, I have not relied upon any statement or representation pertaining to this matter made by AP RANCH or any other person or entity which is hereby released.</p>
+                <p className="about-indemnity mb-3">I WARRANT THAT I HAVE CAREFULLY READ THIS DOCUMENT AND KNOW ITS CONTENTS, AND THAT I AM 18 YEARS OF AGE OR OLDER AND HAVE FULL AUTHORITY TO EXECUTE THIS DOCUMENT AND THAT I HAVE EXECUTED THIS DOCUMENT VOLUNTARILY AND AS MY OWN FREE ACT. I EXECUTE THIS DOCUMENT FULLY INTENDING TO BE BOUND BY ITS TERMS.</p>
+                <p className="about-indemnity mb-3">If the Student/Participant is not eighteen (18) years of age or older, the signature of Student/Participant's parent or legal guardian is required.</p>
+                <p className="about-indemnity mb-3">As parent or legal guardian of the above-mentioned Student/Participant, I agree to and approve the terms of this Release and Indemnity Agreement and consent to the Student/Participant's participation in the Course/Activity and warrant that I have full authority to do so on behalf of myself, the Student/Participant and the Student/Participant's heirs, personal representatives and assigns. I understand and assume the risks of the Student/Participant's participation in the Course/Activity.</p>
+                <p className="about-indemnity mb-3">I, THE UNDERSIGNED, HEREBY FULLY RELEASE AND FOREVER DISCHARGE AP Ranch and all instructors, sponsors, agents, employees, officers, trustees and affiliates of AP Ranch (collectively "AP RANCH"), of and from any and all liability to me, my personal representatives, assigns, heirs and next of kin, from any and all claims, demands, controversies, actions or causes of action, belonging to me or Student/Participant now or in the future for any damage to or loss of Student/Participant's property, and any injury to Student/Participant's person or Student/Participant's death or any one or more of the foregoing, arising directly or indirectly out of Student/Participant's participation for any purpose in the Course/Activity, INCLUDING ANY DAMAGE, LOSS OR INJURY CAUSED BY ANY ACT OR OMISSION ON THE PART OF AP RANCH, INCLUDING ANY NEGLIGENT CONDUCT OF AP RANCH but excluding any gross negligence or willful misconduct of AP RANCH. IT IS MY EXPRESS INTENT THAT THE ABOVE RELEASE INCLUDES THE RELEASE BY ME OF AP RANCH FROM THE CONSEQUENCES OF AP RANCH'S OWN NEGLIGENCE. THE ONLY CIRCUMSTANCE UNDER WHICH MY RELEASE OF AP RANCH DOES NOT APPLY IS WITH RESPECT TO ANY OCCURRENCE RESULTING FROM THE GROSS NEGLIGENCE OR WILLFUL MISCONDUCT OF AP RANCH.</p>
+                <p className="about-indemnity mb-3">I FURTHER AGREE TO INDEMNIFY, DEFEND, AND HOLD HARMLESS AP RANCH for, from and against any and all liabilities, damages, claims, lawsuits, costs (including court costs, attorneys fees and costs of investigation), and actions of any kind or description for any damage to or loss of Student/Participant's property or the property of another, any injury to Student/Participant or any other person, any injury resulting in Student/Participant's death or the death of another or any one or more of the foregoing, arising out of Student/Participant's participation for any purpose in the Course/Activity, INCLUDING ANY DAMAGE, LOSS OR INJURY CAUSED BY ANY ACT OR OMISSION ON THE PART OF AP RANCH, INCLUDING ANY NEGLIGENT CONDUCT OF AP RANCH but excluding any gross negligence or willful misconduct of AP RANCH. IT IS MY EXPRESS INTENT THAT THE ABOVE INDEMNITY INCLUDES INDEMNIFICATION BY ME OF AP RANCH FROM THE CONSEQUENCES OF AP RANCH'S OWN NEGLIGENCE. THE ONLY CIRCUMSTANCE UNDER WHICH MY OBLIGATION TO INDEMNIFY AP RANCH DOES NOT APPLY IS WITH RESPECT TO AN OCCURRENCE RESULTING FROM THE GROSS NEGLIGENCE OR WILLFUL MISCONDUCT OF AP RANCH.</p>
+              </div>
+
+              <form action="" className="row mt-5">
+              <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Age Division</label>
+                      <input type="date" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Signature of Parent or Legal Guardian (print you name)</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                  <button className="send-btn">SEND</button>
+                  </div>
+              </form>
+              
+            </div>
+          </div>
+        </section>
+
+      <Footer />
     </div>
   );
 };
--- src/components/about/indemnity-agreement.tsx
+++ src/components/about/indemnity-agreement.tsx
@@ -1,11 +1,83 @@
 import React from "react";
 import Header from "../layout/header";
+import Footer from "../../pages/Footer";
 
 const IndemnityAgreement = () => {
   return (
     <div>
       <Header />
-      IndemnityAgreement
+        <section className="indemnity-form">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2 className="indemnity-heading">Parent’s Code of Conduct Agreement</h2>
+                <p className="about-indemnity mb-3">Your child is participating in a youth track program at the Athletic Performance Ranch. Participation in this program is very valuable to the development of children. Aspects of teamwork, dedication, discipline, physical fitness, self-esteem and fair play all contribute to the overall growth and maturation of young people. We are an organization whose goals are to instill these values/qualities in all participants regardless of individual athletic ability or the outcome.</p>
+                <p className="about-indemnity">Your role as a spectator has a significant impact upon the success of our program. Therefore, we need you to fully understand and completely accept the standards of behavior detailed below.</p>
+                <div className="indemnity-agreement">
+                  <p className="about-indemnity mb-3">I therefore agree:</p>
+                  <ol>
+                    <li>I will remember that children participate to have fun and that the track program is for youth and not adults.</li>
+                    <li>I understand that children play sports to have fun, make friends, and to learn new skills; therefore, I will praise effort as well as execution in order to keep sports in the proper perspective.</li>
+                    <li>I will inform the coach of any physical disability or special needs that may affect the safety of my child
+                    or the safety of others.</li>
+                    <li>I will learn the rules of the sport and the policies of AP Ranch.</li>
+                    <li>I (and my guests) will be a positive role model for my child and encourage sportsmanship by showingrespect and courtesy, and by demonstrating positive support for all athletes, coaches, officials andspectators at every meet.</li>
+                    <li>I (and my guests) will not engage in any kind of unsportsmanlike conduct with any official, coach,
+                    athlete or parent such as booing or taunting, or using profane language or gestures.</li>
+                    <li>I will not encourage any behaviors or practices that would endanger the health and wellbeing of the
+                    athletes.</li>
+                    <li>I will teach my child to play by the rules and to resolve conflicts without resorting to hostility or
+                    violence.</li>
+                    <li>I will demand that my child treat other athletes, coaches, officials, and spectators with respect.</li>
+                    <li>I will praise my child for competing fairly and trying hard.</li>
+                    <li>I will promote the physical and emotional well-being of the athletes ahead of any personal desire I may
+                    have for my child to win</li>
+                    <li>I will respect the officials and their authority during meets and will never question, discuss or confront
+                    coaches at the meet and will take time to voice my concerns later with the coach.</li>
+                    <li>I will demand a sports environment for my child that is free from drugs, tobacco and alcohol and I will
+                    refrain from their use at all sporting events.</li>
+                  </ol>
+                </div>
+                <p className="about-indemnity" >I fully understand and completely accept the standards of behavior outlined. My signature below will serve as my public acknowledgment of my unconditional agreement with each standard contained in this document.</p>
+                <p className="about-indemnity">I realize that if at any time my behavior becomes non-compliant with any of these standards this could potentially jeopardize my child’s opportunity to participate in the track youth program.</p>
+
+                <form action="" className="pt-5 row">
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Printed Name of Student/Participant</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Age Division</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Age Division</label>
+                      <input type="date" className="contact-from-input" />
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="d-flex flex-column position-relative">
+                      <label  className="contact-form-label">Signature of Parent or Legal Guardian (print you name)</label>
+                      <input type="text" className="contact-from-input" />
+                    </div>
+                  </div>
+                </form>
+
+                <button className="send-btn">SEND</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      <Footer />
     </div>
   );
 };
