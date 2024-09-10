cd492134094c992cc37b29a8f20e238ac8b0a93c
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2024-09-10 12:11:16 +0530

    Designed forms page.

--- src/components/about/forms.tsx
+++ src/components/about/forms.tsx
@@ -1,11 +1,82 @@
 import React from "react";
 import Header from "../layout/header";
+import Footer from "../../pages/Footer";
 
 const Forms = () => {
   return (
     <div>
-      <Header />
-      Forms
+      <div>
+        <Header />
+        <section className="about-banner ">
+          <div className="container-fluid mb-4">
+            <div className="row">
+              <div className="col-lg-6 p-0  our-missions-page">
+                <div className="our-missions-page-content ">
+                  <h1 className="about-collabrate-question">
+                    REGISTRATION NOW OPEN!!!
+                  </h1>
+                  <ul style={{ listStyleType: "num" }}>
+                    <li className="about-page-para">
+                      Fill out the registration form.
+                    </li>
+                    <li className="about-page-para">
+                      Fill out the Indemnity waiver form.
+                    </li>
+                    <li className="about-page-para">
+                      Make payment for selected training, payments can be paid
+                      online at www.apranch.org under “donate”
+                    </li>
+                  </ul>
+                  <p className="about-page-para mt-5">
+                    **Scholarships and grants are also available**
+                  </p>
+                  <p className="about-collabrate-question  mt-5">
+                    AP RANCH Sports AND TRAINING WORKOUTS
+                  </p>
+                  <p className="about-collabrate-question  mt-3">
+                    Training Days :
+                  </p>
+                  <p className="about-page-para  mt-3">
+                    Monday -Thursday 2:00pm-9:00pm
+                  </p>
+                  <p className="about-page-para  mt-3">
+                    Saturday 10:00am-3:00pm
+                  </p>
+                  <p className="about-page-para  mt-3">Sunday: Per Request</p>
+                  <p className="about-page-para  mt-3">
+                    Contact Coach Kae to schedule training: coachkae@apranch.org
+                  </p>
+                  <p className="about-page-para  mt-3">
+                    Location: 1590 RM 2871 Fort Worth, TX 76126
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-lg-5 p-0 our-missions-page  ">
+                <div
+                  className="d-flex justify-content-center flex-wrap "
+                  style={{ gap: "20px" }}
+                >
+                  <button
+                    className="form-white-btn"
+                    style={{ fontFamily: "'Satoshi', sans-serif" }}
+                  >
+                    Indemnity Agreement
+                  </button>
+                  <button className="form-white-btn">
+                    Parent's Code of Conduct Agreement
+                  </button>
+                  <button className="form-white-btn">
+                    Scholarship Application
+                  </button>
+                  <button className="form-white-btn">Grant Application</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/components/about/our-missions.tsx
+++ src/components/about/our-missions.tsx
@@ -1,42 +1,90 @@
 import React from "react";
 import Header from "../layout/header";
-import aboutBanner from "../../assets/img/png/about-banner.png";
+import Footer from "../../pages/Footer";
 
 const OurMissions = () => {
   return (
     <div>
       <Header />
-      <section className="about-banner">
-        <div className="container-fluid">
+      <section className="about-banner ">
+        <div className="container-fluid mb-4">
           <div className="row">
             <div className="col-lg-6 p-0  our-missions-page">
               <div className="our-missions-page-content ">
                 <h1 className="about-page-heading">Our Mission</h1>
-                
+
                 <p className="about-page-para">
-                As a society, wisdom and experience has taught us to view education as the great equalizer. To succeed as individuals and as a nation in an ever-changing and highly-technical world, we believe all youth should aspire to obtaining a college degree. Most people have seen or experienced first-hand that the “one size fits all” approach to success is resulting in far too many youth falling between the cracks, with the majority being youth of all ethnic backgrounds of low socio-economic status.
+                  As a society, wisdom and experience has taught us to view
+                  education as the great equalizer. To succeed as individuals
+                  and as a nation in an ever-changing and highly-technical
+                  world, we believe all youth should aspire to obtaining a
+                  college degree. Most people have seen or experienced
+                  first-hand that the “one size fits all” approach to success is
+                  resulting in far too many youth falling between the cracks,
+                  with the majority being youth of all ethnic backgrounds of low
+                  socio-economic status.
                 </p>
                 <p className="about-page-para">
-                Positive youth development theorists speak about the value of providing youth a voice. We believe that it is equally important to provide them choice. For youth who have access to fewer resources, fewer engaged adults in their lives and less confidence in their capacity to positively influence their environment, the introduction of choice can have a powerful and positive impact on both engagement and retention. The chances of increasing retention and achievement among high school and college students are significantly improved by individual relationships with caring adults who have great expectations and high standards, a sense of membership and positive peer culture and an opportunity to develop academic and athletic skills. Success also depends on attention to the time youth are not in school and recognition of the important role that community-based organizations play in creating a sense of connectedness for them and their families by offering structure for and interest in, their athletic and academic success.
+                  Positive youth development theorists speak about the value of
+                  providing youth a voice. We believe that it is equally
+                  important to provide them choice. For youth who have access to
+                  fewer resources, fewer engaged adults in their lives and less
+                  confidence in their capacity to positively influence their
+                  environment, the introduction of choice can have a powerful
+                  and positive impact on both engagement and retention. The
+                  chances of increasing retention and achievement among high
+                  school and college students are significantly improved by
+                  individual relationships with caring adults who have great
+                  expectations and high standards, a sense of membership and
+                  positive peer culture and an opportunity to develop academic
+                  and athletic skills. Success also depends on attention to the
+                  time youth are not in school and recognition of the important
+                  role that community-based organizations play in creating a
+                  sense of connectedness for them and their families by offering
+                  structure for and interest in, their athletic and academic
+                  success.
                 </p>
                 <p className="about-page-para">
-                Accordingly, we have designed and implemented program strategies that ensure equal access to post- secondary achievement that will increase the number of socio-economically disadvantaged youth enter and flourish in college. We support the creation of alternative learning environments that offer early exposure to the world of work, that combine academic support, and those that bridge secondary and post-secondary educational systems. By expanding the landscape of substantive and rewarding options available to our community’s youth, they will be better equipped with the skills and experience to make informed choices about their future.
+                  Accordingly, we have designed and implemented program
+                  strategies that ensure equal access to post- secondary
+                  achievement that will increase the number of
+                  socio-economically disadvantaged youth enter and flourish in
+                  college. We support the creation of alternative learning
+                  environments that offer early exposure to the world of work,
+                  that combine academic support, and those that bridge secondary
+                  and post-secondary educational systems. By expanding the
+                  landscape of substantive and rewarding options available to
+                  our community’s youth, they will be better equipped with the
+                  skills and experience to make informed choices about their
+                  future.
                 </p>
                 <p className="about-page-para ">
-                The scope of this plan is ambitious and broad. As with any planning document, the strategies and actions that have been set out to accomplish our organization’s goals will require constant monitoring and revisions. Revisions will be based on objective and comprehensive assessment of progress and participant needs. This Plan is much more complex than a traditional organizational strategic plan, as we are attempting to provide programming at a level of service like no other; focusing both on academic enrichment and athletic skill development which will positively impact on our entire community. The vision is compelling and the stakes are high. Our young people and our community members deserve nothing less.
+                  The scope of this plan is ambitious and broad. As with any
+                  planning document, the strategies and actions that have been
+                  set out to accomplish our organization’s goals will require
+                  constant monitoring and revisions. Revisions will be based on
+                  objective and comprehensive assessment of progress and
+                  participant needs. This Plan is much more complex than a
+                  traditional organizational strategic plan, as we are
+                  attempting to provide programming at a level of service like
+                  no other; focusing both on academic enrichment and athletic
+                  skill development which will positively impact on our entire
+                  community. The vision is compelling and the stakes are high.
+                  Our young people and our community members deserve nothing
+                  less.
                 </p>
               </div>
             </div>
 
             <div className="col-lg-6 p-0 our-missions-page  ">
               <div className="d-flex justify-content-center ">
-                  <h3 className="upcoming-event-heading mt-3">Upcoming Events</h3>
+                <h3 className="upcoming-event-heading mt-3">Upcoming Events</h3>
               </div>
             </div>
           </div>
         </div>
       </section>
-
+      <Footer />
     </div>
   );
 };
